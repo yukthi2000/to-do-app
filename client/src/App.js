2217eabb5113fec43bc4a002a81b80c24ec4779b
yukthi hettiarachchi <101061736+yukthi2000@users.noreply.github.com>
2023-06-06 12:45:59 +0530

    backend done

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return <div className="App">
+    <h1>Welcome</h1>
+    <h4>Yout Tasks</h4>
+    
+    
+      </div>;
+}
+
+export default App;
